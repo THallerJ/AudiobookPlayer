a8e45b1e33a63fd9ea2d00aaaae5394e32d8715a
halle189@umn.edu <hallert60@gmail.com>
2021-12-12 20:28:28 -0600

    The playing chapter is now highlighted in the sidebar. Changed secondary color of theme.

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,14 @@ const theme = createTheme({
 	drawer: {
 		width: 240,
 	},
+	palette: {
+		secondary: {
+			light: "#b1b1b1",
+			main: "#9e9e9e",
+			dark: "#6e6e6e",
+			contrastText: "#000",
+		},
+	},
 });
 
 function App() {
--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -14,7 +14,8 @@ import { useGoogle } from "../contexts/GoogleContext";
 
 const Sidebar = () => {
 	const { openDrawer, setOpenDrawer } = useDashboard();
-	const { currentBook, setPlayingChapter, setPlayingBook } = useGoogle();
+	const { currentBook, setPlayingChapter, playingChapter, setPlayingBook } =
+		useGoogle();
 
 	function handleDrawerClose() {
 		setOpenDrawer(false);
@@ -25,6 +26,16 @@ const Sidebar = () => {
 		listItems.push(`Chapter ${i + 1}`);
 	}
 
+	function generateListItemClassName(chapter) {
+		if (playingChapter) {
+			if (!playingChapter.id.localeCompare(chapter.id)) {
+				return "playingChapter";
+			}
+		}
+
+		return null;
+	}
+
 	const drawerContent = (
 		<StyledDrawerContent>
 			<Typography variant="subtitle2" align="center">
@@ -33,9 +44,10 @@ const Sidebar = () => {
 			<Divider />
 			<List>
 				{currentBook
-					? currentBook.chapters.map((chapter, index) => {
+					? currentBook.chapters.map((chapter) => {
 							return (
 								<ListItem
+									className={generateListItemClassName(chapter)}
 									key={chapter.id}
 									divider={true}
 									dense={true}
@@ -96,5 +108,12 @@ const StyledDrawerContent = styled("div")(({ theme }) => ({
 		paddingLeft: theme.spacing(3),
 		paddingTop: theme.spacing(1),
 		color: theme.palette.text.secondary,
+	},
+
+	".playingChapter": {
+		backgroundColor: theme.palette.secondary.light,
+		"&:hover": {
+			backgroundColor: theme.palette.secondary.light,
+		},
 	},
 }));
