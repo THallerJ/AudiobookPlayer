1a0db6967849d8e72f694be81ccb5c1d6d01380e
halle189@umn.edu <hallert60@gmail.com>
2022-02-10 22:22:32 -0600

    Setup server for deploying to Heroku.

--- server/index.js
+++ server/index.js
@@ -39,4 +39,4 @@ app.use("/auth", authRoute);
 app.use("/google", googleRoute);
 app.use("/player", playerRoute);
 
-app.listen(5000, () => console.log(`Server listening on port ${5000}`));
+app.listen(process.env.PORT || 5000);
