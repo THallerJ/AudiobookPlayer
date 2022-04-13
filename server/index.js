249e9dd7b3c1e538f7c40f0ae26d81bceb797048
halle189@umn.edu <hallert60@gmail.com>
2022-04-12 22:06:10 -0500

    server now serves static files

--- server/index.js
+++ server/index.js
@@ -4,10 +4,12 @@ const dbConfig = require("./config/database-config");
 const dbUtils = require("./utils/database-utils");
 const authRoute = require("./routes/auth.js");
 const googleRoute = require("./routes/google.js");
-const { user } = require("./routes/user.js");
+const { userRoute } = require("./routes/user.js");
+const generalRoute = require("./routes/general.js");
 const cors = require("cors");
 require("./config/passport-config");
 const authMiddleware = require("./middleware/auth");
+const path = require("path");
 
 const app = express();
 
@@ -18,15 +20,16 @@ app.use(express.json());
 
 dbUtils.deleteExpiredDocuments();
 
+app.use(express.static(path.join(__dirname, "/../client/build")));
+
 app.use(
 	cors({
 		credentials: true,
-		origin: process.env.CLIENT_URL,
+		origin: process.env.SERVER_URL,
 	})
 );
 
 app.use(function (req, res, next) {
-	res.header("Access-Control-Allow-Origin", process.env.CLIENT_URL);
 	res.header(
 		"Access-Control-Allow-Headers",
 		"Origin, X-Requested-With, Content-Type, Accept"
@@ -41,5 +44,10 @@ app.use(passport.session());
 app.use("/auth", authRoute);
 app.use("/google", authMiddleware.isAuthenticated, googleRoute);
 app.use("/user", authMiddleware.isAuthenticated, userRoute);
+app.use("/general", generalRoute);
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname + "/../client/build/index.html"));
+});
 
 app.listen(process.env.PORT || 5000);
