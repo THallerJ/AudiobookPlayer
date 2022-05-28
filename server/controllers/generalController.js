a69e69d52a82ada95ca0cd9bf0bd3d0ed513b05e
halle189@umn.edu <hallert60@gmail.com>
2022-05-27 22:27:31 -0500

    fixed login error

--- server/controllers/generalController.js
+++ server/controllers/generalController.js
@@ -2,7 +2,7 @@ function getServerUrl(req, res) {
 	if (req.hostname === "localhost") {
 		res.send("http://localhost:5000");
 	} else {
-		res.send(`https://https${req.hostname}`);
+		res.send(`https://${req.hostname}`);
 	}
 }
 
